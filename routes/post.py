b6adcf8879a403976b043f0f750aaa7e16049686
qw1kly <168624106+qw1kly@users.noreply.github.com>
2025-10-04 20:04:00 +0300

    Update post.py

--- routes/post.py
+++ routes/post.py
@@ -1,5 +1,6 @@
 import asyncio
 
+from pydantic import BaseModel
 from fastapi import APIRouter, HTTPException, Depends, Request
 
 from utils.roulette_manager import spin_exchanger
@@ -7,7 +8,11 @@ from utils.roulette_manager import spin_exchanger
 router = APIRouter()
 
 
+class SpinRequest(BaseModel):
+    roullete_id: int 
+    
 @router.post("/spin")
-async def spin_roullete(roullete_id, credentinals: Request):
+async def spin_roullete(roullete_id: SpinRequest, credentinals: Request):
     return await asyncio.create_task(spin_exchanger(id='9090', balance=90008080, roullete_id=id))
+
 
