262c98879d65c3922701f1a28573b406c21e73e3
qw1kly <168624106+qw1kly@users.noreply.github.com>
2025-10-04 19:57:57 +0300

    Update post.py

--- routes/post.py
+++ routes/post.py
@@ -8,5 +8,6 @@ router = APIRouter()
 
 
 @router.post("/spin")
-async def spin_roullete(id, credentinals: Request):
+async def spin_roullete(roullete_id, credentinals: Request):
     return await asyncio.create_task(spin_exchanger(id='9090', balance=90008080, roullete_id=id))
+
