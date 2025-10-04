499fa0816f42d7529aa36b2779a97fb7c825658d
qw1kly <168624106+qw1kly@users.noreply.github.com>
2025-10-04 20:08:59 +0300

    Update post.py

--- routes/post.py
+++ routes/post.py
@@ -13,6 +13,7 @@ class SpinRequest(BaseModel):
     
 @router.post("/spin")
 async def spin_roullete(roullete_id: SpinRequest, credentinals: Request):
-    return await asyncio.create_task(spin_exchanger(id='9090', balance=90008080, roullete_id=id))
+    return await asyncio.create_task(spin_exchanger(id='9090', balance=90008080, roullete_id=roullete_id))
+
 
 
