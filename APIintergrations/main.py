b73cd81a9f4f8b44b13b632da507db565596771c
qw1kly <168624106+qw1kly@users.noreply.github.com>
2025-10-08 12:32:25 +0300

    Update main.py

--- APIintergrations/main.py
+++ APIintergrations/main.py
@@ -19,7 +19,7 @@ def get_client():
 
 async def get_auth_token():
     try:
-        return "tma user=%7B%22id%22%3A7102670717%2C%22first_name%22%3A%22.%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22aze_official_01%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F9xdU5r1S-B6ZuyON8OMPHr_TaTAYPF8zYBA4RKM0k3M4B_8JoC9MDJQ1PJsDTnYM.svg%22%7D&chat_instance=267653665114097988&chat_type=sender&auth_date=1759857755&signature=zeh9l35pYBn4TN17gJxJ995Qs3F7hICrwJElYkp2D_c95ZqlX31OA0jrrr-5pkuIEHbFNFDA81QDi48TGz0pCg&hash=9a587de458efbd50102e029526cfb909505bc51e96034a4a260b91647371b677"
+        return "tma user=%7B%22id%22%3A7102670717%2C%22first_name%22%3A%22.%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22aze_official_01%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F9xdU5r1S-B6ZuyON8OMPHr_TaTAYPF8zYBA4RKM0k3M4B_8JoC9MDJQ1PJsDTnYM.svg%22%7D&chat_instance=267653665114097988&chat_type=sender&auth_date=1759915221&signature=ZmPC5KeP7bHTLpxOqsw0II5IQmwtW04HCa0w330GcJmXKHmWyUqY4V3TKdyHr_6RtgQHSAfkjsh91T_O7H2DAQ&hash=5f408fc149017dede7f51b769b31d3985242c60fdad9489ef6c009f6bbc5a252"
     finally:
         # Не останавливаем клиент, чтобы переиспользовать
         pass
@@ -36,5 +36,6 @@ async def close_client():
         await _client.stop()
 
         _client = None
+
 
 
